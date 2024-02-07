189aad58d0d652bf5979afd28bc77d808701c463
mikeyyj <mikejerez@gmail.com>
2024-02-07 18:03:50 -0500

    fixed css

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,11 @@ export default function Home() {
   return (
     <main>
       <div className="container">
-        {/* nav bar here */}
         <h1 className="navForNow">Nav bar here</h1>
         <Faq />
+        <div className="faqContainer">
+          <p className="titleFaq">FAQs</p>
+        </div>
       </div>
     </main>
   );
